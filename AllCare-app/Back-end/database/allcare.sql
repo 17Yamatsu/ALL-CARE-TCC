7784a7bb0eaeec55596291a312389926927121a3
Gustavo Siqueira <gustavo.asus3@outlook.com>
2026-05-13 20:14:07 -0300

    v2.0.1 em 13/05/2026 às 20:13
    Criada tabela de PROFISSIONAL:
    todos os itens básicos como email, cpf, endereço, id, nome, senha (hash), etc. + itens exclusivos:
    formations - formações acadêmicas e experiência profissional por escrito
    certifications - upload de certificados (JPG, PNG, JPEG, PDF, etc.) e comprovantes
    
    Adicionadas as informações de endereço para paciente, conta do usuário e profissional:
    country - país
    state - estado (opções)
    city - cidade
    streetname - nome da rua
    number - número (opcional, caso não tenha número - recomendável adicionar botão "Sem número" na tela de cadastro)
    cep - CEP
    neighborhood - bairro
    
    Também foi adicionado a coluna de número de telefone "phonenumber" que estava faltando.
    
    Criada a tabela de ligação de usuário e profissional: "usr_pro"

--- AllCare-app/Back-end/database/allcare.sql
+++ AllCare-app/Back-end/database/allcare.sql
@@ -6,25 +6,61 @@ CREATE TABLE usuario (
 	usr_id int primary key auto_increment,
 	usr_name VARCHAR(120) not null,
     usr_mail VARCHAR(120) not null,
-    usr_birthday date not null,
-    usr_cpf varchar(11) not null,
-    usr_address varchar(300) not null,
-    usr_cep varchar(8) not null,
+    usr_phonenumber int not null,
+    usr_birhday date not null,
+    usr_cpf int not null,
+    usr_address_country varchar(300) not null,
+    usr_address_state enum("AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","TO") not null,
+    usr_address_city varchar(300) not null,
+    usr_adress_streetname varchar(600) not null,
+    usr_adress_cep int not null,
+    usr_address_number int,
+    usr_address_type enum("Casa","Apartamento/Condomínio") default("Casa") not null,
+    usr_address_neighborhood varchar(300) not null,
     usr_type ENUM("Paciente", "Usuário") default("Usuário"),
     usr_pwd VARCHAR(16) not null,
     usr_photo mediumblob,
-    --  vusr_medicalinfoarchar(700) not null
+    usr_medicalinfo varchar(700) not null
 );
 
 create table paciente (
 	pac_id int primary key auto_increment,
 	pac_name varchar(120) not null,
-    pac_address varchar(300) not null,
-    pac_cpf varchar(11) not null,
+    pac_address_country varchar(300) not null,
+    pac_address_state enum("AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","TO") not null,
+    pac_address_city varchar(300) not null,
+    pac_adress_streetname varchar(600) not null,
+    pac_adress_cep int not null,
+    pac_address_number int,
+    pac_address_type enum("Casa","Apartamento/Condomínio") default("Casa") not null,
+    pac_address_neighborhood varchar(300) not null,
+    pac_cpf int not null,
     pac_birthday date not null,
-    -- pac_medicalinfo varchar(700) not null
+    pac_medicalinfo varchar(700) not null,
+    pac_photo mediumblob
 );
 
+
+create table profissional (
+	pro_id int primary key auto_increment,
+    pro_name varchar(300) not null,
+    pro_mail varchar(300) not null,
+    pro_pwd varchar(16) not null,
+    pro_address_country varchar(300) not null,
+    pro_address_state enum("AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","TO") not null,
+    pro_address_city varchar(300) not null,
+    pro_adress_streetname varchar(600) not null,
+    pro_adress_cep int not null,
+    pro_address_number int,
+    pro_address_type enum("Casa","Apartamento/Condomínio") default("Casa") not null,
+    pro_address_neighborhood varchar(300) not null,
+    pro_birthday date not null,
+    pro_cpf int not null,
+    pro_phonenumber int not null,
+    pro_formations varchar(2455) not null,
+    pro_certifications mediumblob not null,
+    pro_photo mediumblob not null
+);
 
 create table pac_usr (
 	FK_usr int,
@@ -33,3 +69,16 @@ create table pac_usr (
     foreign key (FK_usr) references usuario(usr_id),
     foreign key (FK_pac) references paciente(pac_id)
 );
+
+create table usr_pro (
+	FK_pro int,
+    FK_usr int,
+    primary key (FK_pro, FK_usr),
+    foreign key (FK_pro) references profissional(pro_id),
+    foreign key (FK_usr) references usuario(usr_id)
+);
+
+
+#drop table usuario;
+#drop table pac_usr;
+#drop table paciente;
